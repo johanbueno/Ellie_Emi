61a4156ec09ee6fd7347c1e0dd314beaae818516
Johan Bueno <johan.s.bueno@gmail.com>
2020-08-18 23:37:19 -0400

    Nav

--- elli_emi/src/App.js
+++ elli_emi/src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {Component} from "react";
+import { Route } from 'react-router-dom';
+import Home from "./Home";
+import Profile from "./Profile";
 
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  render (){
+    return (
+      <>
 
-       
-    </div>
-  );
+      <Route path="/" exact component={Home}/>
+      <Route path="/profile" exact component={Profile}/>
+      </>
+    )
+  }
 }
 
 export default App;
+
+
---
+++ elli_emi/src/Nav.js
@@ -0,0 +1,19 @@
+import React, { Component, useLayoutEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+class Nav extends Component {
+    render() {
+        return (
+           <ul>
+               <li>
+
+               </li>
+               <li>
+                   
+               </li>
+           </ul>
+        );
+    }
+}
+
+export default Nav;
