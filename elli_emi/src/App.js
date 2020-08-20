5bb3b76db806127c371b8715116acde8383ee89c
Johan Bueno <johan.s.bueno@gmail.com>
2020-08-19 21:33:05 -0400

    Autho-js

--- elli_emi/src/App.js
+++ elli_emi/src/App.js
@@ -2,15 +2,18 @@ import React, {Component} from "react";
 import { Route } from 'react-router-dom';
 import Home from "./Home";
 import Profile from "./Profile";
+import Nav from './Nav'
 
 
 class App extends Component {
   render (){
     return (
       <>
-
+      <Nav/>
+      <div className='body'>
       <Route path="/" exact component={Home}/>
       <Route path="/profile" exact component={Profile}/>
+      </div>
       </>
     )
   }
--- elli_emi/src/Nav.js
+++ elli_emi/src/Nav.js
@@ -4,14 +4,16 @@ import { Link } from 'react-router-dom';
 class Nav extends Component {
     render() {
         return (
+            <nav>
            <ul>
                <li>
-
+                <Link to="/">Home</Link>
                </li>
                <li>
-                   
+               <Link to="/profile">Profile</Link>
                </li>
            </ul>
+           </nav>
         );
     }
 }
