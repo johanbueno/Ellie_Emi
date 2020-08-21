bc664645a4a78b3dc62c6fa50214dc1eea02a205
Johan Bueno <johan.s.bueno@gmail.com>
2020-08-20 23:02:58 -0400

    handleAuthentication

--- elli_emi/src/App.js
+++ elli_emi/src/App.js
@@ -3,15 +3,28 @@ import { Route } from 'react-router-dom';
 import Home from "./Home";
 import Profile from "./Profile";
 import Nav from './Nav'
+import Auth from "./Auth/Auth";
+import Callback from "./Callback";
 
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.auth = new Auth(this.props.history);
+  }
   render (){
     return (
       <>
       <Nav/>
       <div className='body'>
-      <Route path="/" exact component={Home}/>
+      <Route path="/"
+      exact 
+      render={props => <Home auth={this.auth} {...props} />}
+      />
+       <Route path="/callback"
+      exact 
+      render={props => <Callback auth={this.auth} {...props} />}
+      />
       <Route path="/profile" exact component={Profile}/>
       </div>
       </>
--- elli_emi/src/Auth/Auth.js
+++ elli_emi/src/Auth/Auth.js
@@ -9,6 +9,36 @@ export default class Auth {
             redirectUri: process.env.REACT_APP_AUTH0_REDIRECT_URL,
             responseType: "token id_token",
             scope:"openid profile email"
-        })
+        });
     }
+    login = () => {
+        this.auth0.authorize();
+    };
+
+    handleAuthentication = () => {
+        this.auth0.parseHash((err, authResult) =>{
+            if (authResult && authResult.accessToken && authResult.idToken){
+                this.setSession(authResult);
+                this.history.push("/");
+            } else if (err) {
+                this.history.push("/");
+                alert (`Error: ${err.error}. Check the console`);
+                console.log(err);
+            }
+        }) 
+    }
+    setSession = authResult => {
+        //set time the access token will expire 
+        const expiresAt = JSON.stringify(
+            authResult.expiresAt * 1000 + new Date().getTime()
+        );
+        localStorage.setItem("access_token", authResult.accessToken);
+        localStorage.setItem("id_token", authResult.idToken);
+        localStorage.setItem("expires_at", expiresAt);
+    };
+    isAuthenticated() {
+        const expiresAt = JSON.parse(localStorage.getItem("expires_at"));
+        return new Date().getTime() < expiresAt;
+    }
+    
 }
