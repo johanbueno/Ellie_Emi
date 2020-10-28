53752d15c4594179657ddc88199f360c6070c132
Johan Bueno <johan.s.bueno@gmail.com>
2020-10-27 22:20:39 -0400

    PageNotFound

--- elli_emi/src/App.js
+++ elli_emi/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Home from "./Home";
 import Profile from "./Profile";
 import Nav from "./Nav";
@@ -9,6 +9,7 @@ import Public from "./Public";
 import Private from "./Private";
 import Footer from "./Footer";
 import Bows from "./Bows";
+import PageNotFound from "./PageNotFound";
 
 class App extends Component {
   constructor(props) {
@@ -20,37 +21,40 @@ class App extends Component {
       <>
         <Nav auth={this.auth} />
         <div className="body">
-          <Route
-            path="/"
-            exact
-            render={(props) => <Home auth={this.auth} {...props} />}
-          />
-          <Route
-            path="/callback"
-            render={(props) => <Callback auth={this.auth} {...props} />}
-          />
-          <Route
-            path="/profile"
-            render={(props) =>
-              this.auth.isAuthenticated() ? (
-                <Profile auth={this.auth} {...props} />
-              ) : (
-                <Redirect to="/" />
-              )
-            }
-          />
-          <Route path="/public" component={Public} />
-          <Route path="/bows" component={Bows} />
-          <Route
-            path="/private"
-            render={(props) =>
-              this.auth.isAuthenticated() ? (
-                <Private auth={this.auth} {...props} />
-              ) : (
-                this.auth.login()
-              )
-            }
-          />
+          <Switch>
+            <Route
+              path="/"
+              exact
+              render={(props) => <Home auth={this.auth} {...props} />}
+            />
+            <Route
+              path="/callback"
+              render={(props) => <Callback auth={this.auth} {...props} />}
+            />
+            <Route
+              path="/profile"
+              render={(props) =>
+                this.auth.isAuthenticated() ? (
+                  <Profile auth={this.auth} {...props} />
+                ) : (
+                  <Redirect to="/" />
+                )
+              }
+            />
+            <Route path="/public" component={Public} />
+            <Route path="/bows" component={Bows} />
+            <Route
+              path="/private"
+              render={(props) =>
+                this.auth.isAuthenticated() ? (
+                  <Private auth={this.auth} {...props} />
+                ) : (
+                  this.auth.login()
+                )
+              }
+            />
+            <Route component={PageNotFound} />
+          </Switch>
           <Footer />
         </div>
       </>
