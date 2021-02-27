554e552eb0355315ed5f4ae62367b5b843ad0b20
Johan Bueno <johan.s.bueno@gmail.com>
2021-02-26 22:40:49 -0500

    dollar-price

--- elli_emi/src/components/Products.js
+++ elli_emi/src/components/Products.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import formatCurrency from "../util";
 
 export default class Products extends Component {
   render() {
@@ -14,7 +15,7 @@ export default class Products extends Component {
                 </a>
               </div>
               <div className="product-price">
-                <div>{product.price}</div>
+                <div>{formatCurrency(product.price)}</div>
                 <button className="button primary">Add To Cart</button>
               </div>
             </li>
